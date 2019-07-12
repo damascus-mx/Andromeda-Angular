26ed4bda55e2be62262a9f7053b5964ff115bc9a
Alonso R <luis.alonso.16@hotmail.com>
2019-07-11 22:12:36 -0600

    Add material design modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+/**
+ * @name Andromeda
+ * @version 0.0.1a
+ * @copyright Damascus. 2019 All rights reserved.
+ * @license Confidential This file belongs to Damascus IT intellectual property,
+ * any unauthorized distribution of this file will be punished by law.
+ * @author Alonso Ruiz
+ * @description Andromeda's IoC lazyloaded-modules
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialUtils } from './utils/material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialUtils
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/utils/material.module.ts
@@ -0,0 +1,77 @@
+/**
+ * @name Andromeda
+ * @version 0.0.1a
+ * @copyright Damascus. 2019 All rights reserved.
+ * @license Confidential This file belongs to Damascus IT intellectual property,
+ * any unauthorized distribution of this file will be punished by law.
+ * @author Alonso Ruiz
+ * @description Material Bundler
+ */
+import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatBadgeModule,
+    MatTabsModule,
+    MatStepperModule,
+    MatBottomSheetModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatSlideToggleModule
+} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+
+
+@NgModule({
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatChipsModule,
+        MatProgressSpinnerModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatMenuModule,
+        MatBadgeModule,
+        MatSelectModule,
+        MatTabsModule,
+        MatStepperModule,
+        MatBottomSheetModule,
+        MatSlideToggleModule,
+        LayoutModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CdkStepperModule,
+    ]
+})
+
+export class MaterialUtils { }
