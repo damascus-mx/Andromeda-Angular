8930a7addffeb11f37f7289f51321a1d770caa86
Alonso R <luis.alonso.16@hotmail.com>
2019-07-30 21:39:37 -0600

    Add infinite scroll in likes container

--- src/app/pages/shared/components/post/components/likes/likes.component.ts
+++ src/app/pages/shared/components/post/components/likes/likes.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { PostRepository } from 'src/app/pages/home/api/post.repository';
 import { Post } from 'src/app/domain/models/post.model';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 export interface DialogLikesData {
   Id: any;
@@ -14,19 +15,52 @@ export interface DialogLikesData {
 })
 export class DialogLikesComponent implements OnInit {
   post: Post;
+  likes: any[] = [];
   selectedUser: any = null;
+
+  // Scrolling
+  private currentPage: number;
+  private maxPage: number;
+  @ViewChild(CdkVirtualScrollViewport, { static: false }) viewport: CdkVirtualScrollViewport;
 
   constructor(
     public dialogRef: MatDialogRef<DialogLikesComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogLikesData, private postRepository: PostRepository) {
+      this.currentPage = 0;
     }
 
   ngOnInit(): void {
-    !this.data ? this.data.Id = 'undefined' : this.post = this.postRepository.GetById(this.data.Id);
+    !this.data ? this.data.Id = 'undefined' : this.fetchData();
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  fetchData(): void {
+    this.post = this.postRepository.GetById(this.data.Id);
+    this.maxPage = this.post.likes.length;
+    this.fetchLikes();
+  }
+
+  fetchLikes(): void {
+    let limit = 1;
+    for (let i = this.currentPage; i < this.maxPage; i++ ) {
+      if ( limit <= 3 ) {
+        this.likes.push(this.post.likes[i]);
+        limit++;
+      } else {
+        continue;
+      }
+    }
+  }
+
+  onScroll(): void {
+    if (this.currentPage <= this.maxPage - 1) {
+      this.currentPage += 3;
+      this.fetchLikes();
+      console.log('Get likes');
+    }
   }
 
 }
